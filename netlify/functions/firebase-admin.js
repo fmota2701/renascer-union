f1a41c3e5497b4716a0fd5b8352196e9223a1f08
Renascer Union Admin <admin@renascer-union.com>
2025-08-29 23:23:55 -0300

    Configuração Firebase para produção - chave privada em base64

--- netlify/functions/firebase-admin.js
+++ netlify/functions/firebase-admin.js
@@ -8,7 +8,7 @@ if (!admin.apps.length) {
     type: "service_account",
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    private_key: process.env.FIREBASE_PRIVATE_KEY_BASE64 ? Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64, 'base64').toString('utf8') : process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
