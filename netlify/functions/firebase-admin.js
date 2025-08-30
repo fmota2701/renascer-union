aaea2be47db4aa434e63d3842ac3dbe5adf5b290
Renascer Union Admin <admin@renascer-union.com>
2025-08-29 23:50:07 -0300

    Correção da configuração Firebase Admin - simplificação para resolver erros 500

--- netlify/functions/firebase-admin.js
+++ netlify/functions/firebase-admin.js
@@ -3,23 +3,16 @@ const admin = require('firebase-admin');
 
 // Inicializar Firebase Admin apenas se ainda não foi inicializado
 if (!admin.apps.length) {
-  // Para produção, use variáveis de ambiente
+  // Configuração simplificada usando apenas as variáveis essenciais
   const serviceAccount = {
     type: "service_account",
     project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY_BASE64 ? Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64, 'base64').toString('utf8') : process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+    client_email: process.env.FIREBASE_CLIENT_EMAIL
   };
 
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`
+    credential: admin.credential.cert(serviceAccount)
   });
 }
 
